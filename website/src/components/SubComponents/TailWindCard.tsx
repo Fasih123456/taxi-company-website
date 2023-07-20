83a96cfe43db36692971e65772dde5b7b0619caf
Fasih <fasihislam1999@gmail.com>
2023-07-20 14:52:58 -0300

    updated website with changes

--- website/src/components/SubComponents/TailWindCard.tsx
+++ website/src/components/SubComponents/TailWindCard.tsx
@@ -23,7 +23,7 @@ const TailWindCard: React.FC<CardTypeProps> = ({ imgSrc, children, ...props }) =
         alt=""
         className="transition-transform group-hover:scale-110 transform duration-200 max-w-sm tailwind-card-image"
       />
-      <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent">
+      <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent inner-card">
         <div
           className={`p-4 text-white ${shouldAnimate ? "animate__animated animate__fadeInUp" : ""}`}
         >
